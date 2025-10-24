fb04b9428e0c9c5f0f9c7ac0a8c3947d6d269a0a
ben-io7 <ben.o@giritoday.com>
2025-10-24 23:04:42 +0200

    Updated Notes.js file with JS arrays

--- Notes.js
+++ Notes.js
@@ -92,11 +92,76 @@ if(has_pen && has_paper){
 // console.log(obj['c'])
 
 //Nested loops
-let letters = ['a', 'b', 'c']
-let numbers = [1, 2, 3]
+// let letters = ['a', 'b', 'c']
+// let numbers = [1, 2, 3]
 
-for (let i = 0; i < letters.length; i++){
-   for(let j = 0; j < numbers.length; j++){
-      console.log(`this is outer loop variable ${letters[i]} and this is inner loop variable ${numbers[j]}`)
+// for (let i = 0; i < letters.length; i++){
+//    for(let j = 0; j < numbers.length; j++){
+//       console.log(`this is outer loop variable ${letters[i]} and this is inner loop variable ${numbers[j]}`)
+//    }
+// }
+
+//Arrays
+
+let colors = ['red', 'green', 'blue']
+//access the first element
+// console.log(colors[0]);
+
+// let green = colors[1]
+// console.log(green);
+
+// colors[1] = 'yellow';
+// console.log(colors);
+
+//create an empty array
+// let fruits = []
+// fruits.push('apple');
+// fruits.push('banana');
+// console.log(fruits[1]);
+
+//loop through an array
+//let numbers_one = [1,2,3,4,5,6]
+
+//for loop
+// for(let i = 0; i < number_one.length; i++){
+//    console.log(number_one[i])
+// }
+
+// let evenNumbers = numbers_one.filter(function(number){
+//    return number % 2 === 0;
+// })
+
+// console.log(evenNumbers)
+
+//for each
+// colors.forEach(function(color){
+//    console.log(color);
+// });
+
+//Join
+// let words = ['Hello', 'world']
+// let sentence = words.join(' ')
+// console.log(sentence)
+
+//Slice
+
+// let numbers =[ 0,1,2,3,4,5]
+// let slicedNumbers = numbers.slice(1, 3)
+// console.log(slicedNumbers)
+
+// Create a two-dimensional array
+let matrix = [
+   [1,2,3],
+   [4,5,6],
+   [7,8,9]
+]
+
+let firstRow_firstNumber = matrix[2][2]
+console.log(firstRow_firstNumber);
+
+//loop through two dimentional array
+
+for ( let i = 0; i< matrix.length; i++ ) //loop through the row, outer loop
+   for (let j = 0; j < matrix[i].length; j++){
+      console.log(matrix[i][j])
    }
-}
